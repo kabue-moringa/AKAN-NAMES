b8a7ac61cbf845ad19234a5097d06158aa26faf2
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-04-15 15:09:46 +0300

    add function result

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 var dayNames = ["Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday"];
 var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwao", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+function results () {
