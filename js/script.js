6ee65f50179323ef6af85c861d19c3fb39249935
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-04-15 15:11:27 +0300

    add var day

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ var dayNames = ["Sunday", "Monday", "Tuesday", "Wednessday", "Thursday", "Friday
 var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwao", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 function results () {
+    var day = parseInt( document.getElementById("day").value);
+    var month = parseInt( document.getElementById("month").value);
+    var year = parseInt( document.getElementById("year").value);
